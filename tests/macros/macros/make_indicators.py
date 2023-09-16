3396898ee65cad6ae870af11bcae1aa8a44cb10c
datnguye <datnguyen.it09@gmail.com>
2023-09-16 13:15:21 +0700

    fix: W291 trailing whitespace

--- tests/macros/macros/make_indicators.py
+++ tests/macros/macros/make_indicators.py
@@ -5,7 +5,7 @@ from sqlmesh import macro
 def make_indicators(evaluator, string_column, string_values):
     return [
         f"""CASE
-            WHEN {string_column} = {value} THEN {value} 
+            WHEN {string_column} = {value} THEN {value}
             ELSE CAST(NULL as VARCHAR)
         END AS {string_column.name}_{value.name}"""
         for value in string_values.expressions
