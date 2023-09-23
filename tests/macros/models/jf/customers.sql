5e7085aeccc9708bd8161242ef5ab660442afd0e
datnguye <datnguyen.it09@gmail.com>
2023-09-23 12:37:41 +0700

    fix: integration test

--- tests/macros/models/jf/customers.sql
+++ tests/macros/models/jf/customers.sql
@@ -7,7 +7,7 @@ MODEL (
 
 
 CREATE SCHEMA IF NOT EXISTS common; /* prepare mp */
-CREATE OR REPLACE TABLE common.log (
+CREATE TABLE IF NOT EXISTS common.log (
   id VARCHAR
 ); /* for faking mp */
 @create_masking_policy(common.mp_first_name, None, TRUE); /* @create_masking_policy(common.mp_first_name); */
